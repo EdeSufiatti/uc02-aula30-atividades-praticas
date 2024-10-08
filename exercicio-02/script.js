1dfee220c883f210b6a8782e99e3c5a34398adb9
Ede Sufiatti <sufiattie@gmail.com>
2024-10-07 21:57:06 -0300

    c4

--- exercicio-02/script.js
+++ exercicio-02/script.js
@@ -1,3 +1,26 @@
+const min = 1
+const max = 100
+const resultado = Math.floor(Math.random() * (max - min + 1)) + min
+
+const btnCalcular = document.getElementById("btn-calcular")
 const divRoot = document.getElementById("root")
-const btnCalcular = document.getElementById('btn-calcular')
-const inputValor1 = Number(document.getElementById("valor1").value);
+const textResultado = document.createElement("h3")
+divRoot.appendChild(textResultado)
+
+console.log(resultado)
+
+btnCalcular.addEventListener("click", (event) => {
+    event.preventDefault()
+    // ENTRADA
+    // Pegar os dois valores e a operação
+    const inputNumeroAposta = document.getElementById("numero-aposta")
+    const numeroApostado = Number(inputNumeroAposta.value)
+
+    if (numeroApostado > resultado) {
+        textResultado.textContent = "Errouuuuu! É menos bicho..."
+    } else if (numeroApostado < resultado) {
+        textResultado.textContent = "Errouuuuu! É mais bicho..."
+    } else {
+        textResultado.textContent = "O loco bicho! Acertou!"
+    }
+})
